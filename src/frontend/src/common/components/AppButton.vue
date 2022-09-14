1cb63a779a930bb96a356c33150e12356077a155
alistkov <antonlistkov@gmail.com>
2022-09-14 22:06:47 +0300

    вынес кнопку и инпут в отдельные компоненты

---
+++ src/frontend/src/common/components/AppButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <button v-bind="$attrs" class="button"><slot></slot></button>
+</template>
+
+<script>
+export default {
+  name: "AppButton",
+};
+</script>
---
+++ src/frontend/src/common/components/AppInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <label class="input">
+    <slot></slot>
+    <input :type="type" :name="name" :placeholder="placeholder" />
+  </label>
+</template>
+
+<script>
+export default {
+  name: "AppInput",
+
+  props: {
+    type: {
+      type: String,
+      default: "text",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
