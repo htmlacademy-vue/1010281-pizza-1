c48acaed0dff7e62fc80ff667b34254ddbfed369
alistkov <antonlistkov@gmail.com>
2022-12-20 00:27:39 +0300

    create drag and drops components

---
+++ src/frontend/src/common/components/AppDrag.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>App drag component</div>
+</template>
+
+<script>
+export default {
+  name: "AppDrag",
+};
+</script>
---
+++ src/frontend/src/common/components/AppDrop.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>App drop component</div>
+</template>
+
+<script>
+export default {
+  name: "AppDrop",
+};
+</script>
